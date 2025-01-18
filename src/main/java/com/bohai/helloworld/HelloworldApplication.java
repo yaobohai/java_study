540f41aed656c55d00b6e9d5384206a9c3242dd9
姚博海 <9803680+yaobohai@user.noreply.gitee.com>
2025-01-19 02:44:52 +0800

    modify

--- src/main/java/com/bohai/helloworld/HelloworldApplication.java
+++ src/main/java/com/bohai/helloworld/HelloworldApplication.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
-import com.ctrip.framework.apollo.ConfigService;
 
 @SpringBootApplication
 @RestController
